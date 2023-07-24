df542dc8faca40f86cc1c16e6561104deba0a39e
Manali Thakkar <manalithakkar735@gmail.com>
2023-07-24 17:05:52 +0530

    Initial setup

---
+++ iNews/iNews/APICaller.swift
@@ -0,0 +1,8 @@
+//
+//  APICaller.swift
+//  iNews
+//
+//  Created by manali thakkar on 24/07/23.
+//
+
+import Foundation
---
+++ iNews/iNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iNews
+//
+//  Created by manali thakkar on 24/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "News"
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
